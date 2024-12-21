ae9b9278a52da8fed3ce37844c4b24393e74a750
talhagonec <talhagonec@gmail.com>
2024-12-21 13:05:28 +0300

    handle_redirection

--- src/shell.c
+++ src/shell.c
@@ -59,7 +59,7 @@ void execute_sequence(char *input)
 //  Girilen komutu çalıştırır (örn: "ls", "cat", vb.)
 void execute_command(char *command)
 {
-    //Harun Yesilyurt G211210089
+    // Harun Yesilyurt G211210089
     char *args[MAX_ARGS];
     int i = 0;
     int background = 0;
@@ -122,9 +122,39 @@ void execute_command(char *command)
     }
 }
 
-// TODO
 //  Komut içindeki dosya yönlendirmesini işler (">", "<" gibi)
-void handle_redirection(char **args) { /* ... */ }
+void handle_redirection(char **args)
+{
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], "<") == 0)
+        {
+            // Giriş yönlendirme
+            int fd = open(args[i + 1], O_RDONLY);
+            if (fd < 0)
+            {
+                perror("Giriş dosyası açılamadı");
+                exit(EXIT_FAILURE);
+            }
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+            args[i] = NULL;
+        }
+        else if (strcmp(args[i], ">") == 0)
+        {
+            // Çıkış yönlendirme
+            int fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd < 0)
+            {
+                perror("Çıkış dosyası açılamadı");
+                exit(EXIT_FAILURE);
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+            args[i] = NULL;
+        }
+    }
+}
 
 // TODO
 //  Pipe'lı (|) komutları işler
