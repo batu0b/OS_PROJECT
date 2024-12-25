24f5cea1f071f77f5e353d0fbc9461c55d4c04da
Ömer Faruk Aydın <leviverleihtflugel@gmail.com>
2024-12-25 03:48:48 +0300

    Update main.c
    Grup üyelerinin adları ve grup adı eklendi

--- src/main.c
+++ src/main.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include "shell.h"
 
+//GRUP ADI: 12AB Grubu
+//GRUP UYELERI
+//Ömer Faruk Aydın - G221210370 - 1.Öğretim A Grubu
+//Tarık Kartal - Y245012016 - 1. Öğretim A Grubu
+//Harun Mahmut Yeşilyurt - G211210089 - 2. Öğretim B Grubu
+//Batuhan Bahayetmez - G211210043 - 2. Öğretim A Grubu
+//Talha İmran Göneç - G221210032 - 2. Öğretim B Grubu
+
 int main()
 {
     char input[MAX_INPUT];
